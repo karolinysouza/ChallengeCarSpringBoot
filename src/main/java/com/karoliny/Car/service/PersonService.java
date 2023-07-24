d0527d14c893d86a9cf410f636038b561f882842
Karoliny Souza <karolinytech@gmail.com>
2023-07-24 19:47:57 -0300

    feat: Added methods registerDb (registers data in the database) and getCarById (working ID not found exception).

--- src/main/java/com/karoliny/Car/service/PersonService.java
+++ src/main/java/com/karoliny/Car/service/PersonService.java
@@ -1,11 +1,49 @@
 package com.karoliny.Car.service;
 
+import com.karoliny.Car.dto.PersonDtoRequest;
+import com.karoliny.Car.dto.PersonDtoResponse;
+import com.karoliny.Car.entity.Person;
+import com.karoliny.Car.exception.PersonNotFoundExeption;
+import com.karoliny.Car.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PersonService {
-    public String getString() {
-        return "dentro do service";
+
+    @Autowired
+    PersonRepository personRepository;
+
+        public void registerDb(PersonDtoRequest personDtoRequest) {
+
+        Person person = new Person(
+                null,
+                personDtoRequest.getName(),
+                personDtoRequest.getBrand(),
+                personDtoRequest.getColor(),
+                personDtoRequest.getFabricationYear());
+        personRepository.save(person);
+
     }
 
+    public PersonDtoResponse getCarById(Long idChassi) {
+        Optional<Person> optionalPerson = personRepository.findById(idChassi);
+
+        if (optionalPerson.isPresent()) {
+            Person person = optionalPerson.get();
+
+            PersonDtoResponse personDtoResponse = new PersonDtoResponse(
+                    person.getIdChassi(),
+                    person.getName(),
+                    person.getBrand(),
+                    person.getColor(),
+                    person.getFabricationYear());
+
+            return personDtoResponse;
+        } else {
+            throw new PersonNotFoundExeption("Car not found!");
+        }
+    }
 }

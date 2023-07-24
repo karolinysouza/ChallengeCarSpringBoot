52ec3a166e516082f1d27890070d3f3c4d5fb545
Karoliny Souza <karolinytech@gmail.com>
2023-07-24 19:53:26 -0300

    feat: Added getter, setter and constructor.

--- src/main/java/com/karoliny/Car/dto/PersonDtoResponse.java
+++ src/main/java/com/karoliny/Car/dto/PersonDtoResponse.java
@@ -1,4 +1,60 @@
 package com.karoliny.Car.dto;
 
 public class PersonDtoResponse {
+    private Long idChassi;
+    private String name;
+    private String brand;
+
+    private String color;
+
+    private String fabricationYear;
+
+
+    public PersonDtoResponse(Long idChassi, String name, String brand, String color, String fabricationYear) {
+        this.idChassi = idChassi;
+        this.name = name;
+        this.brand = brand;
+        this.color = color;
+        this.fabricationYear = fabricationYear;
+    }
+
+    public Long getIdChassi() {
+        return idChassi;
+    }
+
+    public void setIdChassi(Long idChassi) {
+        this.idChassi = idChassi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFabricationYear() {
+        return fabricationYear;
+    }
+
+    public void setFabricationYear(String fabricationYear) {
+        this.fabricationYear = fabricationYear;
+    }
 }

534f25d59d6cf0748f79a0663b8bfe539ac6ec32
Karoliny Souza <karolinytech@gmail.com>
2023-07-25 10:30:22 -0300

    feat: Implemented class extending CarException for the registerDb method exception.

---
+++ src/main/java/com/karoliny/Car/exception/InvalidBrandException.java
@@ -0,0 +1,8 @@
+package com.karoliny.Car.exception;
+
+public class InvalidBrandException extends CarException {
+    public InvalidBrandException(String message) {
+
+        super(message);
+    }
+}
